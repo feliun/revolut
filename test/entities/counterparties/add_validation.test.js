5d18b4d2d2d1af348f6554d1720816ffbf2d6c19
feliun <felipe.polo@gmail.com>
2017-12-16 13:04:31 +0000

    Validating new UK counterparties

--- test/entities/counterparties/add_validation.test.js
+++ test/entities/counterparties/add_validation.test.js
@@ -1,9 +1,11 @@
+const R = require('ramda');
 const { OK } = require('http-status-codes');
 const expect = require('expect.js');
 const nock = require('nock');
 const initRevolut = require('../../..');
 
 const counterparty_response = require('../../fixtures/counterparties/counterparty_response.json');
+const uk_account = require('../../fixtures/counterparties/uk_account.json');
 
 // Based on https://revolutdev.github.io/business-api/?shell--sandbox#counterparties
 
@@ -43,6 +45,17 @@ describe('Validation for new counterparties', () => {
       return Promise.resolve()
         .then(() => add(revolutAccount))
         .catch((error) => expect(error.message).to.equal('ValidationError: child "name" fails because ["name" must be a string]'));
+    });
+  });
+
+  describe('Validation for new UK Bank Account users', () => {
+    it('POSTs a new valid UK bank account counterparty', () => add(uk_account));
+
+    it('fails to add an invalid UK Bank Account', () => {
+      const faultyAccount = R.omit(['account_no'], uk_account);
+      return Promise.resolve()
+        .then(() => add(faultyAccount))
+        .catch((error) => expect(error.message).to.equal('ValidationError: child "account_no" fails because ["account_no" is required]'));
     });
   });
 });
