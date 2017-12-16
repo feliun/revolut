71ed6d55bcd4689a354899e65a46d8a983778ee9
feliun <felipe.polo@gmail.com>
2017-12-16 13:25:23 +0000

    Enriching entities with debug info

--- entities/accounts.js
+++ entities/accounts.js
@@ -1,9 +1,15 @@
+const debug = require('debug')('revolut:accounts');
+
 module.exports = ({ url, request }) => ({
   // GET https://b2b.revolut.com/api/1.0/accounts
-  getAll: () => request.get(`${url}/accounts`),
+  getAll: () => {
+    debug('Getting all accounts');
+    return request.get(`${url}/accounts`);
+  },
   // GET https://b2b.revolut.com/api/1.0/accounts/<id>
   get: (accountId) => {
     if (!accountId) throw new Error('You need to provide an account ID.');
+    debug(`Getting account ${accountId}`);
     return request.get(`${url}/accounts/${accountId}`);
   },
 });
--- entities/counterparties.js
+++ entities/counterparties.js
@@ -1,21 +1,30 @@
+const debug = require('debug')('revolut:counterparties');
 const validate = require('../lib/validators/counterparties');
 
 module.exports = ({ url, request }) => {
   // POST https://b2b.revolut.com/api/1.0/counterparty
-  const add = (counterparty) => request.post(`${url}/counterparty`, validate(counterparty));
+  const add = (counterparty) => {
+    debug('Adding a new counterparty');
+    return request.post(`${url}/counterparty`, validate(counterparty));
+  };
 
   // DELETE https://b2b.revolut.com/api/1.0/counterparty/<id>
   const remove = (counterpartyId) => {
     if (!counterpartyId) throw new Error('You need to provide a counterparty ID.');
+    debug(`Deleting counterparty ${counterpartyId}`);
     return request.remove(`${url}/counterparty/${counterpartyId}`);
   };
 
   // GET https://b2b.revolut.com/api/1.0/counterparties
-  const getAll = () => request.get(`${url}/counterparties`);
+  const getAll = () => {
+    debug('Getting all counterparties');
+    return request.get(`${url}/counterparties`);
+  };
 
   // GET https://b2b.revolut.com/api/1.0/counterparty/<id>
   const get = (counterpartyId) => {
     if (!counterpartyId) throw new Error('You need to provide a counterparty ID.');
+    debug(`Getting counterparty ${counterpartyId}`);
     return request.get(`${url}/counterparty/${counterpartyId}`);
   };
 
